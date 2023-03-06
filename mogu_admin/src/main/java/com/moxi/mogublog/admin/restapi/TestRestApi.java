a51c51cc1e745d6f2ac76db136308a7bec69508d
zc_macpro <11>
2023-03-06 15:44:01 +0800

    add gateway config

--- mogu_admin/src/main/java/com/moxi/mogublog/admin/restapi/TestRestApi.java
+++ mogu_admin/src/main/java/com/moxi/mogublog/admin/restapi/TestRestApi.java
@@ -2,6 +2,7 @@ package com.moxi.mogublog.admin.restapi;
 
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +13,23 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class TestRestApi {
 
+    @Value("${server.port}")
+    private String port ;
+
     @GetMapping("/test")
     public String test(String val){
         log.info("val的值:" + val);
         return val ;
+    }
+
+    /**
+     * 测试网关接口转发轮询lb
+      * @return
+     */
+    @GetMapping("/lb")
+    public String lb(){
+        log.info("server port:" + port);
+        return "server port:" + port ;
     }
 
 
